e92ce842209d909ff6b0a08f86ccd0d182755e16
matt <vigneronmatthieu@hotmail.com>
2021-10-21 11:26:59 +0100

    add jsonapi-rails

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,8 @@ class Message < ApplicationRecord
   belongs_to :card
 
   validates_presence_of :card_id, :content
+
+  # necessary?
+  # validates_presence_of :user_id, if: Proc.new { |message| !message.user_id })
+  # validates_presence_of :name, if: Proc.new { |message| !message.name })
 end
