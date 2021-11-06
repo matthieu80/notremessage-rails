dfb215e51c042400359affa18abe4d627638da52
matt <vigneronmatthieu@hotmail.com>
2021-11-06 10:39:59 +0000

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,17 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://your.frontend.domain.com'
-    resource '/api/*',
-      headers: %w(Authorization),
-      methods: :any,
-      expose: %w(Authorization),
-      max_age: 600
+    origins '*'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :patch, :put],
+      expose: %w(Authorization)
   end
+  # allow do
+  #   origins 'http://localhost:3001'
+  #   resource '/api/*',
+  #     headers: %w(Authorization),
+  #     methods: :any,
+  #     expose: %w(Authorization),
+  #     max_age: 600
+  # end
 end
